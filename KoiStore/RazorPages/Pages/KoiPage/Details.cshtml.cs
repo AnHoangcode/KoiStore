b9fbf393706a54114938050fb9afdd7feed5e65a
Chau Quoc Bao <quocbaoo2883@gmail.com>
2024-11-13 10:33:29 +0700

    Update KoiProfile

--- KoiStore/RazorPages/Pages/KoiPage/Details.cshtml.cs
+++ KoiStore/RazorPages/Pages/KoiPage/Details.cshtml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using BusinessObjects.Models;
-using DAOs;
 using Services.Interface;
 
 namespace RazorPages.Pages.KoiPage
@@ -16,6 +11,7 @@ namespace RazorPages.Pages.KoiPage
         private readonly IKoiProfileService _koiProfileService;
         private readonly IKoiFarmService _koiFarmService;
         private readonly IKoiTypeService _koiTypeService;
+
         public DetailsModel(IKoiProfileService koiProfileService, IKoiFarmService koiFarmService, IKoiTypeService koiTypeService)
         {
             _koiProfileService = koiProfileService;
@@ -24,8 +20,12 @@ namespace RazorPages.Pages.KoiPage
         }
 
         [BindProperty]
-
         public KoiProfile KoiProfile { get; set; } = default!;
+
+        [BindProperty]
+        public int Quantity { get; set; }
+
+        public string? ErrorMessage { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -46,6 +46,29 @@ namespace RazorPages.Pages.KoiPage
                 KoiProfile = koiprofile;
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostBuyNowAsync(int id, int quantity)
+        {
+            var koiprofile = _koiProfileService.GetKoiProfileById(id);
+
+            if (koiprofile == null)
+            {
+                return NotFound();
+            }
+
+            if (quantity > koiprofile.Quantity)
+            {
+                ErrorMessage = "Out of stock";
+                return await OnGetAsync(id); // Trả về trang hiện tại với thông báo lỗi
+            }
+
+            // Xử lý logic mua hàng (giảm số lượng trong kho và tạo đơn hàng nếu cần)
+            koiprofile.Quantity -= quantity;
+            //await _koiProfileService.UpdateKoiProfileAsync(koiprofile);
+
+            // Điều hướng đến trang xác nhận hoặc giỏ hàng
+            return RedirectToPage("/KoiPage/OrderConfirmation", new { id, quantity });
         }
     }
 }
